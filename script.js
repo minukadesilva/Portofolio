b879fdccfaea0c90affc6a1bd0118dcbeacf1b83
Minuka De Silva <100896047+minukadesilva@users.noreply.github.com>
2025-08-06 16:06:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+//Hamburger section
+
 const hamburger = document.getElementById("hamburger");
+const closeIcon = document.getElementById("close-icon");
 const navLinks = document.getElementById("nav-links");
 const socialIcons = document.getElementById("social-icons");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("show");
   socialIcons.classList.toggle("show");
+  hamburger.classList.add("hide");
+  closeIcon.classList.add("show");
 });
+
+closeIcon.addEventListener("click", () => {
+  navLinks.classList.remove("show");
+  socialIcons.classList.remove("show");
+  closeIcon.classList.remove("show");
+  hamburger.classList.remove("hide");
+});
+
+//Contact alert section
 
 document
   .getElementById("contact-form")
